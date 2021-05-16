edcbaf69453393c46aa24402dcf8b4dba93c4f36
oliverhoang <82502436+oliverhoang@users.noreply.github.com>
2021-05-17 02:37:57 +0800

    Update script.js

--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,12 @@
+/**
+ * Javascript file contains all of the functions require for the game to work
+ */
+
+/**
+ * Class MemoGame with contrucstor which takes totalTime and cards array as parameters
+ * @param totalTime the time limit for the game
+ * @param cards the card array which contains all of the cards in an array
+ */
 class MemoGame {
     constructor(totalTime, cards) {
         this.cardsArray = cards;
@@ -7,35 +16,56 @@ class MemoGame {
         this.ticker = document.getElementById('flips');
     }
 
+    /**
+     * Function to initialise the game
+     */
     startGame() {
         this.totalClicks = 0;
         this.timeRemaining = this.totalTime;
         this.cardToCheck = null;
         this.matchedCards = [];
         this.busy = true;
+
+        /**
+         * Function to give a short break before loading the game
+         * make the transition between the game page to other page and vice versa smoothly
+         */
         setTimeout(() => {
             this.shuffleCards();
             this.countDown = this.startCountDown();
             this.busy = false;
         }, 700);
+
+        /**
+         * Display the back card face
+         * Display original time remaing and number of flips
+         */
         this.hideCards();
         this.timer.innerText = this.timeRemaining;
         this.ticker.innerText = this.totalClicks; 
     }
 
-    shuffleCards() { // Fisher-Yates Shuffle Algorithm.
-        /*document.getElementById("test").innerHTML= this.cardsArray.length
-        document.getElementById("test1").innerHTML= this.cardsArray[0]*/
-
+    /**
+     * Function to shuffle the position of the cards in the array
+     * Swap the index of each card with a random index of another card in the array
+     * using the Fisher–Yates shuffle algorithm
+     */
+    /*
+    shuffleCards() { 
         for (let i = this.cardsArray.length - 1; i > 0; i--) {
             let randIndex = Math.floor(Math.random() * (i + 1));
             this.cardsArray[randIndex].style.order = i;
             this.cardsArray[i].style.order = randIndex;
         }
-        
-
     }
+    */
 
+    /**
+     * Function to return the time remaining and display on web page,
+     * is called after every interval
+     * also check if the game is over given the time remaining
+     * @returns time remaining
+     */
     startCountDown() {
         return setInterval(() => {
             this.timeRemaining--;
@@ -45,6 +75,13 @@ class MemoGame {
         }, 1000);
     }
 
+    /**
+     * Function to display the front card face
+     * by making the front card face visible
+     * increment clicks if the user click on a card that can be fliped
+     * assign this card to a variable to check for match with the next card
+     * @param card the card that is being clicked by the player
+     */
     flipCard(card) {
         if(this.canFlipCard(card)) {
             this.totalClicks++;
@@ -59,13 +96,23 @@ class MemoGame {
         }
     }
 
+    /**
+     * Function to display the back card face for every card
+     * by remove the visible attribute of the front card
+     */
     hideCards() {
         this.cardsArray.forEach(card => {
             card.classList.remove('visible');
-            card.classList.remove('matched');
         });
     }
     
+    /**
+     * Function to check if 2 cards are the same
+     * by comparing the source path of the two images that are being checked
+     * add matched cards to a matched card array
+     * otherwise flip back the cards
+     * @param {*} card 
+     */
     checkForCardMatch(card) {
         if(this.getCardType(card) === this.getCardType(this.cardToCheck))
             this.cardMatch(card, this.cardToCheck);
@@ -74,16 +121,37 @@ class MemoGame {
 
         this.cardToCheck = null;
     }
+
+    /**
+     * Function to get the source path of the image that is assigned to the current card
+     * @param card the current card
+     * @returns the source path of the image
+     */
+    getCardType(card) {
+        return card.getElementsByClassName('card-front-value')[0].src;
+    }
+
+    /**
+     * Function to push the matched cards to an array
+     * game is over if the array contain every card
+     * @param card1 first matched card
+     * @param card2 second matched card
+     */
     cardMatch(card1, card2) {
         this.matchedCards.push(card1);
         this.matchedCards.push(card2);
-        /*card1.classList.add('matched');
-        card2.classList.add('matched');*/
-    
+
         if(this.matchedCards.length === this.cardsArray.length)
             this.gameOver();
     }
 
+    /**
+     * Function to flip back the cards if they are not the matched
+     * block the ability to flip the card during this process
+     * 
+     * @param {*} card1 the first card
+     * @param {*} card2 the second card
+     */
     cardMismatch(card1, card2) {
         this.busy = true;
         setTimeout(() => {
@@ -93,10 +161,11 @@ class MemoGame {
         }, 1000);
     }
 
-    getCardType(card) {
-        return card.getElementsByClassName('card-front-value')[0].src;
-    }
-
+    /**
+     * Function to give user the result after the game is over
+     * containing time taken, number of flips, and number of pairs correct
+     * @returns number of flips and time taken
+     */
     gameOver() {
         alert("You have made " + this.totalClicks + " flips\nGot " + this.matchedCards.length/2 + " pair(s) correct\nTime taken: "  + (this.totalTime - this.timeRemaining) +'s');
         clearInterval(this.countDown);
@@ -105,18 +174,29 @@ class MemoGame {
         return (this.totalClicks, this.totalTime - this.timeRemaining);
     }
 
+    /**
+     * Function to check if the current card can be fliped
+     * @param card the current card
+     * @returns true if can be flip, false otherwise
+     */
     canFlipCard(card) {
-        
         return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck);
     }
 }
 
+/**
+ * Function to be called when the this javascript file is loaded completely
+ */
 function ready() {
     let cards = Array.from(document.getElementsByClassName('card'));
     let game = new MemoGame(100, cards);
 
     game.startGame();
 
+    /**
+     * for each card, addEventListener of type click
+     * for each click, pass the cards to flipCard function
+     */
     cards.forEach(card => {
         card.addEventListener('click', () => {
             game.flipCard(card);
@@ -124,8 +204,12 @@ function ready() {
     });
 }
 
+/**
+ * If the page is not loaded yet,
+ * wait for the page to load before run the code
+ */
 if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready);
+    document.addEventListener('DOMContentLoaded', ready());
 } else {
     ready();
 }
