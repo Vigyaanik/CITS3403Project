666024bcfbdd9e8315fc0e46a842a29fe5bdf90b
oliverhoang <82502436+oliverhoang@users.noreply.github.com>
2021-05-16 11:56:02 +0800

    Update routes.py

--- Content/app/routes.py
+++ Content/app/routes.py
@@ -8,3 +8,12 @@ def index():
 @app.route('/getGame/<int:numColls>/<int:numRows>/<string:folderName>/<int:test>')
 def getGame(numColls,numRows,folderName,test):
     return render_template('game.html', numColls=numColls, numRows=numRows,folderName=folderName,test=test)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(500):
+    dp.session.rollback()
+    return render_template('500.html'), 500
